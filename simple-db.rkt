e7804c1200c94bb1ae3fa700d35d922df2de53e0
Mithaecus <hey@eddy.ninja>
2022-04-09 14:09:33 -0700

    Add load/save

--- simple-db.rkt
+++ simple-db.rkt
@@ -1,22 +1,5 @@
 #lang racket
 
-(define db%
-    (class object%
-        (init-field [records '()])
-        (define/public (add-record cd)
-            (begin 
-                (if (empty? records)
-                    (set! records cd)
-                    (set! records (cons cd records)))))
-        (define/public (display-all)
-            (for/last ([cd (flatten (list records))])
-                (printf "Title:  ~a\nArtist: ~a\nRating: ~a\nRipped: ~a\n\n" 
-                    (get-field title cd) 
-                    (get-field artist cd) 
-                    (get-field rating cd) 
-                    (get-field ripped cd))))
-    (super-new)))
-
 (define cd%
     (class object%
         (init-field title artist rating ripped)
@@ -25,10 +8,45 @@
 (define (make-cd title artist rating ripped)
     (new cd% [title title] [artist artist] [rating rating] [ripped ripped]))
 
+(define db%
+    (class object%
+        (init-field [records '()])
+        (define/public (add-record cd)
+            (begin 
+                (if (empty? records)
+                    (set! records cd)
+                    (set! records (cons cd records)))))
+        (define/public (dump-all)
+            (open-output-file "./db" #:exists 'replace)
+            (for/last ([cd (flatten (list records))])
+                (write (
+                    list (get-field title cd) 
+                         (get-field artist cd) 
+                         (get-field rating cd) 
+                         (get-field ripped cd)) 
+                    (open-output-file "./db" #:exists 'append))
+            (close-output-port (open-output-file "./db" #:exists 'append))))
+        (define/public (load)
+            (let ([input (port->string (open-input-file "./db"))])
+                (for/last ([cd (input)])
+                    (displayln (string->list (car cd) (caar cd) (caaar cd) (caaaar cd)))
+                    (add-record (make-cd (string->list cd))))))
+    (super-new)))
+
 (define *DB* (make-object db%))
 
-
 (let loop ()
+
+    (display "Load? ")
+    (define load? (read-line (current-input-port) 'any))
+
+    (if (or (equal? load? "Y")
+            (equal? load? "Yes")
+            (equal? load? "y")
+            (equal? load? "yes"))
+        (send *DB* load)
+        (void))
+
     (display "Let's add a CD!\n")
     (display "Title: ")
     (define title (read-line (current-input-port) 'any))
@@ -45,6 +63,16 @@
     (define cd (make-cd title artist rating ripped))
 
     (send *DB* add-record cd)
-    (send *DB* display-all)
+    (send *DB* dump-all)
+    
+    (display "Quit? ")
+    (define quit (read-line (current-input-port) 'any))
+
+    (if (or (equal? quit "N")
+            (equal? quit "No")
+            (equal? quit "n")
+            (equal? quit "no"))
+        (void)
+        (exit))
 
     (loop))
